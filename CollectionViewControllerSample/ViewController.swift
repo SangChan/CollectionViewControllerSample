f984586be8627b19425887e57e8166066aab4b12
SangChan Lee <sangchan82@gmail.com>
2017-01-19 17:18:36 +0800

    left align, dynamic size cell, blahblah

--- CollectionViewControllerSample/ViewController.swift
+++ CollectionViewControllerSample/ViewController.swift
@@ -8,10 +8,13 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UICollectionViewController {
 
+    @IBOutlet weak var flowLayout: UICollectionViewLeftAlignedLayout!
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.title = "Collection Test"
+        self.flowLayout.estimatedItemSize = CGSize(width: 150, height: 70)
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -19,7 +22,141 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 7
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "InfoCell", for: indexPath) as! InfoCell
+        cell.contentLabel.text = randomString()
+        cell.layer.borderColor = UIColor.darkGray.cgColor
+        cell.layer.borderWidth = 1
+        cell.layer.cornerRadius = 8 // optional
+        return cell
+    }
+    
+    func randomString() -> String {
+        let random = arc4random()
+        if random%4 == 0 {
+            return "Lorem Ipsum!"
+            
+        } else if random%4 == 1{
+            return "Lorem Ipsum, Disney Land rules,"
+            
+        } else if random%4 == 2 {
+            return "Lorem Ipsum, Lotte World sucks, Everland sucks too."
+        }
+        return "Lorem Ipsum Psedou Blah Blah Something."
+    }
 
 }
 
+class InfoCell : UICollectionViewCell {
+    @IBOutlet weak var contentLabel: UILabel!
+    var isHeightCalculated: Bool = false
+    
+    override func preferredLayoutAttributesFitting(_ layoutAttributes: UICollectionViewLayoutAttributes) -> UICollectionViewLayoutAttributes {
+        //Exhibit A - We need to cache our calculation to prevent a crash.
+        if !isHeightCalculated {
+            setNeedsLayout()
+            layoutIfNeeded()
+            let size = contentView.systemLayoutSizeFitting(layoutAttributes.size)
+            var newFrame = layoutAttributes.frame
+            newFrame.size.width = CGFloat(ceilf(Float(size.width)))
+            layoutAttributes.frame = newFrame
+            isHeightCalculated = true
+        }
+        return layoutAttributes
+
+    }
+}
+
+extension UICollectionViewLayoutAttributes {
+    func leftAlignFrameWithSectionInset(_ sectionInset:UIEdgeInsets){
+        var frame = self.frame
+        frame.origin.x = sectionInset.left
+        self.frame = frame
+    }
+}
+
+class UICollectionViewLeftAlignedLayout: UICollectionViewFlowLayout {
+
+    override func layoutAttributesForElements(in rect: CGRect) -> [UICollectionViewLayoutAttributes]? {
+        var attributesCopy: [UICollectionViewLayoutAttributes] = []
+        if let attributes = super.layoutAttributesForElements(in: rect) {
+            attributes.forEach({ attributesCopy.append($0.copy() as! UICollectionViewLayoutAttributes) })
+        }
+        
+        for attributes in attributesCopy {
+            if attributes.representedElementKind == nil {
+                let indexpath = attributes.indexPath
+                if let attr = layoutAttributesForItem(at: indexpath) {
+                    attributes.frame = attr.frame
+                }
+            }
+        }
+        return attributesCopy
+    }
+    
+    override func layoutAttributesForItem(at indexPath: IndexPath) -> UICollectionViewLayoutAttributes? {
+        
+        if let currentItemAttributes = super.layoutAttributesForItem(at: indexPath as IndexPath)?.copy() as? UICollectionViewLayoutAttributes {
+            let sectionInset = self.evaluatedSectionInsetForItem(at: indexPath.section)
+            let isFirstItemInSection = indexPath.item == 0
+            let layoutWidth = self.collectionView!.frame.width - sectionInset.left - sectionInset.right
+            
+            if (isFirstItemInSection) {
+                currentItemAttributes.leftAlignFrameWithSectionInset(sectionInset)
+                return currentItemAttributes
+            }
+            
+            let previousIndexPath = IndexPath.init(row: indexPath.item - 1, section: indexPath.section)
+            
+            let previousFrame = layoutAttributesForItem(at: previousIndexPath)?.frame ?? CGRect.zero
+            let previousFrameRightPoint = previousFrame.origin.x + previousFrame.width
+            let currentFrame = currentItemAttributes.frame
+            let strecthedCurrentFrame = CGRect.init(x: sectionInset.left,
+                                                    y: currentFrame.origin.y,
+                                                    width: layoutWidth,
+                                                    height: currentFrame.size.height)
+            // if the current frame, once left aligned to the left and stretched to the full collection view
+            // widht intersects the previous frame then they are on the same line
+            let isFirstItemInRow = !previousFrame.intersects(strecthedCurrentFrame)
+            
+            if (isFirstItemInRow) {
+                // make sure the first item on a line is left aligned
+                currentItemAttributes.leftAlignFrameWithSectionInset(sectionInset)
+                return currentItemAttributes
+            }
+            
+            var frame = currentItemAttributes.frame
+            frame.origin.x = previousFrameRightPoint + evaluatedMinimumInteritemSpacing(at: indexPath.section)
+            currentItemAttributes.frame = frame
+            return currentItemAttributes
+            
+        }
+        return nil
+    }
+    
+    func evaluatedMinimumInteritemSpacing(at sectionIndex:Int) -> CGFloat {
+        if let delegate = self.collectionView?.delegate as? UICollectionViewDelegateFlowLayout {
+            let inteitemSpacing = delegate.collectionView?(self.collectionView!, layout: self, minimumInteritemSpacingForSectionAt: sectionIndex)
+            if let inteitemSpacing = inteitemSpacing {
+                return inteitemSpacing
+            }
+        }
+        return self.minimumInteritemSpacing
+        
+    }
+    
+    func evaluatedSectionInsetForItem(at index: Int) ->UIEdgeInsets {
+        if let delegate = self.collectionView?.delegate as? UICollectionViewDelegateFlowLayout {
+            let insetForSection = delegate.collectionView?(self.collectionView!, layout: self, insetForSectionAt: index)
+            if let insetForSectionAt = insetForSection {
+                return insetForSectionAt
+            }
+        }
+        return self.sectionInset
+    }
+}
